37c4b17081d5ba8f97e1fc72b6b7668c518b8fed
Rifatx990 <158224402+Rifatx990@users.noreply.github.com>
2025-12-17 20:46:55 +0600

    Create server.js

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+// server.js
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+import authRoutes from "./routes/auth.js";
+import orderRoutes from "./routes/orders.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/orders", orderRoutes);
+
+app.listen(5000, () => console.log("Backend running on port 5000"));

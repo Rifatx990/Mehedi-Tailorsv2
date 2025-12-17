7e176998760b014df76e6e5078ceb5079a7c13b2
Rifatx990 <158224402+Rifatx990@users.noreply.github.com>
2025-12-17 21:17:45 +0600

    Create emailService.js

---
+++ backend/src/utils/emailService.js
@@ -0,0 +1,20 @@
+// utils/emailService.js
+import nodemailer from "nodemailer";
+
+export const sendEmail = async (to, subject, text) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: 587,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+  });
+
+  await transporter.sendMail({
+    from: `"Mehedi Tailors" <${process.env.SMTP_USER}>`,
+    to,
+    subject,
+    text
+  });
+};

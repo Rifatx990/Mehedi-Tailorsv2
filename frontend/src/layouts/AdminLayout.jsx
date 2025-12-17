26f0af0c6a74c893da26be8044b4019f3d43c33a
Rifatx990 <158224402+Rifatx990@users.noreply.github.com>
2025-12-17 20:52:58 +0600

    Create AdminLayout.jsx

---
+++ frontend/src/layouts/AdminLayout.jsx
@@ -0,0 +1,11 @@
+// layouts/AdminLayout.jsx
+import AdminSidebar from "../components/admin/AdminSidebar";
+
+export default function AdminLayout({ children }) {
+  return (
+    <div className="flex min-h-screen bg-gray-100">
+      <AdminSidebar />
+      <main className="flex-1 p-6">{children}</main>
+    </div>
+  );
+}
